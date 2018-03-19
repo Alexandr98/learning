b02c739f6a5302a8a47a96166c37ff2c53597431
Alexandr98 <sashaprim98@gmail.com>
2018-03-19 08:55:45 -0700

    fix conflict

--- src/loops/evenNumbers.js
+++ src/loops/evenNumbers.js
@@ -1,8 +1,10 @@
 export function evenNum(i, a) {
+  var arr = [];
   for (i = 0; i < a; i++) {
     if (i % 2 === 0) {
-      console.log(i);
+      arr.push(i);
     }
+    return i;
   }
 }
 evenNum(0, 10);
